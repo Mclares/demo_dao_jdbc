1d7fbba5c336d7d856e10fd75e90852814aac9ec
Marcelo Clares <msc.09.wylde@gmail.com>
2020-06-10 18:07:29 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,10 +32,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 4: seller insert ===");
-		Seller newseller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newseller);
-		System.out.println("Inserted! New id = " + newseller.getId());
+		//System.out.println("\n=== TEST 4: seller insert ===");
+		//Seller newseller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		//sellerDao.insert(newseller);
+		//System.out.println("Inserted! New id = " + newseller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Updated!");
 	
 	}
 	
